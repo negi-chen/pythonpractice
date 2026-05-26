ae50bd50ddaf07d2879f0e0c619a32ce741b4a4d
negi-chen <p-chen@u-group.co.jp>
2026-05-26 17:52:58 +0900

    Add new Python exercises for conditional statements and input handling

---
+++ python/D3.py
@@ -0,0 +1,117 @@
+# Hackerrank
+# Python If-Else
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    # print(n)
+    
+    # for n in range(n):
+    if n % 2 == 0:
+        if n >= 2 and n <= 5:
+            print("Not Weird") 
+        if n >= 6 and n <= 20:
+            print("Weird")
+        if n > 20 :
+            print("Not Weird") 
+    elif n % 2 != 0: 
+        print("Weird")
+
+
+
+#problem 1
+# paiza de java and python
+「1 つの文字列を出力 」
+
+#1.java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        // String line = sc.nextLine();
+        System.out.println("paiza");
+    }
+}
+
+
+#2.python
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+# input_line = input()
+print("paiza")
+
+
+
+
+#problem 2
+期待する出力
+答えの文字列を 1 行で出力してください。
+
+paiza learning
+
+末尾に改行を入れ、余計な文字、空行を含んではいけません。
+
+
+#1.java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        // String line = sc.nextLine();
+        System.out.println("paiza learning");
+    }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        // String line = sc.nextLine();
+        System.out.println("813");
+    }
+}
+
+
+#2.python
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+# input_line = input()
+print("paiza learning")
+
+# ==========================================================
+# 【Python3】標準入力の書き方に困ったらこちら！
+# 
+# 「入力される値」の取得方法一覧（Python）
+# https://paiza.jp/pages/works/cheatsheet/stdin_python
+# ==========================================================
+# ここからコードを書き始めてください
+# input_line = input()
+print("813")
