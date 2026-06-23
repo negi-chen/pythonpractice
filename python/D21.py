0949681bd1db2bfe5fdef2fee8bae42c0590aecf
negi-chen <p-chen@u-group.co.jp>
2026-06-23 10:38:18 +0900

    add yesterdays code

--- python/D21.py
+++ python/D21.py
@@ -66,3 +66,86 @@ for r, row_content in enumerate(grid):
                         answer = "Yes"
 
 print(answer)
+
+
+
+#ans from paiza
+h, w, sy, sx, m = input().split()
+s = [list(input()) for _ in range(int(h))]
+sy = int(sy)
+sx = int(sx)
+
+if m == "N":
+    sy -= 1
+elif m == "E":
+    sx += 1
+elif m == "S":
+    sy += 1
+elif m == "W":
+    sx -= 1
+
+if sx < 0 or sx >= int(w) or sy < 0 or sy >= int(h) or s[sy][sx] == "#":
+    print("No")
+else:
+    print("Yes")
+
+
+
+#java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line[] = sc.nextLine().split(" ");
+        
+        int di1 = Integer.parseInt(line[0]);
+        int di2 = Integer.parseInt(line[1]);
+        int anc3 = Integer.parseInt(line[2]);
+        int anc4 = Integer.parseInt(line[3]);
+        String target = line[4];
+        
+        //we change here let the char in then it will be more accurate
+        String[] grid = new String[di1];
+        for (int k = 0; k < di1; k++) {
+            grid[k] = sc.next();
+        }
+
+        String ans = "No";
+        
+        // for her we run two loop in order to find the right position
+        for (int i = 0; i < di1; i++) {
+            for (int j = 0; j < di2; j++) {
+                if (anc3 == i && anc4 == j) {
+                    
+                    if (target.equals("E")) {
+                        if (j + 1 < di2) {
+                            if (grid[i].charAt(j + 1) != '#') {
+                                ans = "Yes";
+                            }
+                        }
+                    } else if (target.equals("W")) {
+                        if (j - 1 >= 0) {
+                            if (grid[i].charAt(j - 1) != '#') {
+                                ans = "Yes";
+                            }
+                        }
+                    } else if (target.equals("S")) {
+                        if (i + 1 < di1) {
+                            if (grid[i + 1].charAt(j) != '#') {
+                                ans = "Yes";
+                            }
+                        }
+                    } else if (target.equals("N")) {
+                        if (i - 1 >= 0) {
+                            if (grid[i - 1].charAt(j) != '#') {
+                                ans = "Yes";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
